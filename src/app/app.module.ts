190fd984010a148371dfc5272c0c589febe70d5c
Alexis Provo <alexis.provo@ynov.com>
2024-06-18 00:15:41 +0200

    add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
